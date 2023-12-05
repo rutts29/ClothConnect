3741ff62f8454591f02361297d89c36d7d8ef9c0
Ruttansh Bhatelia <bhatelia@sheridancollege.ca>
2023-12-04 19:28:58 -0500

    Add project files.

---
+++ ClothConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClothConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
