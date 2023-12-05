51a2fdf6595c8b0df30dad8ed337854590c805b3
Ruttansh Bhatelia <bhatelia@sheridancollege.ca>
2023-12-05 16:29:56 -0500

    Producty Category CRUD operations for ADMIN dashboard. Initial CSS using Bootstrap

---
+++ ClothConnect/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿
+using ClothConnect.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClothConnect.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet <Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Stiped Shirt", DisplayOrder = 5 },
+                new Category { Id = 2, Name = "Denim Jacket", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "White Sneakers", DisplayOrder = 3 }
+                );
+        }
+
+    }
+}
