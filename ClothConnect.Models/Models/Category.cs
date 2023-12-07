3b7dd20f03ac39e2b07abe3b62228db546a24925
munjalr16 <109973241+munjalr16@users.noreply.github.com>
2023-12-06 21:47:51 -0500

    Update Category.cs

--- ClothConnect.Models/Models/Category.cs
+++ ClothConnect.Models/Models/Category.cs
@@ -1,17 +1,32 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+
 namespace ClothConnect.Models
 {
+    /// <summary>
+    /// Represents a Category entity in the system.
+    /// </summary>
     public class Category
     {
-        [Key]
+        /// <summary>
+        /// Gets or sets the unique identifier for the Category.
+        /// </summary>
+        [Key] // Indicates that this property is the primary key in the database
         public int Id { get; set; }
-        [Required]
-        [MaxLength(25)]
-        [DisplayName("Category Name")]
+
+        /// <summary>
+        /// Gets or sets the name of the Category. The name is required and its length must be 25 characters or less.
+        /// </summary>
+        [Required] // Indicates that this property is required
+        [MaxLength(25)] // Sets the maximum length of the Name to 25 characters
+        [DisplayName("Category Name")] // Sets a user-friendly display name for the property
         public string Name { get; set; }
-        [DisplayName("Display Order")]
-        [Range(1,200)]
+
+        /// <summary>
+        /// Gets or sets the display order of the Category. The value must be between 1 and 200.
+        /// </summary>
+        [DisplayName("Display Order")] // Sets a user-friendly display name for the property
+        [Range(1, 200)] // Sets the allowable range of values for the DisplayOrder from 1 to 200
         public int DisplayOrder { get; set; }
     }
 }
